2770abd45e6c0162ba6e96fc09bc6a0b0c1471cc
jer bushau <exjerlock@hotmail.com>
2017-11-12 18:51:16 -0500

    Added Goma!

--- code_lou_todo/todos/views.py
+++ code_lou_todo/todos/views.py
@@ -31,19 +31,20 @@ class TodoDetail(generic.DetailView):
     # get just entries create just before and just after
     def get_context_data(self, *args, **kwargs):
         context = super(TodoDetail, self).get_context_data(*args, **kwargs)
-        # figure out how to use annotate!!!! seems like it would help here
+
         if context['todo'].is_complete == True:
             all_todos = models.Todo.objects.all().filter(is_complete=True)
         else:
             all_todos = models.Todo.objects.all().filter(is_complete=False)
-        context['todo'].content = context['todo'].content.strip()
+
         context['before'] = all_todos.filter(
-            created_at__gt=context['todo'].created_at).order_by(
-                'created_at')[:1]
+            created_at__gt=context['todo'].created_at
+        ).order_by('created_at')[:1]
+
         context['after'] = all_todos.filter(
-            created_at__lt=context['todo'].created_at).order_by(
-                '-created_at')[:1]
-        #find out if todo has been edited
+            created_at__lt=context['todo'].created_at
+        ).order_by('-created_at')[:1]
+
         if context['todo'].created_at.replace(microsecond=0) != context['todo'].modified_at.replace(microsecond=0):
             context['modded'] = True
         return context
